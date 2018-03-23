856bc782129bd171a874f89597f7bb9eef6ceed7
sunqing <1803839841@qq.com>
2018-03-23 18:16:50 +0800

    1
    1

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,62 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    count: 0,
+    reGet:false,
+    phone: '',
+    userInputCode: '',
+    countDownNum:''
   },
+  // 键盘输入时，触发此事件
+  bindPhoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  bindVerificationCodeInput: function (e) {
+    this.setData({
+      userInputCode: e.detail.value
+    })
+  },
+  setTime: function () {
+    let that = this
+    if (this.data.count == 0) {
+      this.data.reGet = true
+      this.data.countDownNum = ''
+      this.setData({
+        reGet: this.data.reGet,
+        countDownNum: this.data.countDownNum
+      })
+      return
+    }else{
+      this.data.countDownNum = this.data.count + 's后重试'
+      this.data.count--
+      this.setData({
+        countDownNum: this.data.countDownNum
+      })
+    }
+    setTimeout(function () {
+      that.setTime()
+    }, 1000)
+  },
+  getCode: function(){
+    if (this.data.phone.length == 11){
+      if (this.data.count > 0) {
+        return
+      }else{
+        this.data.count = 60
+        this.setTime()
+      }
+    }else{
+      wx.showModal({
+        content: '请输入手机号',
+        showCancel: false,
+        confirmText: '确定',
+      })
+    }
+  },
+  postRequest: function () {
+    let that = this
+    wx.request({
+      url: 'https://31388152.qcloud.la/welsh/app/main/home',
+      data: {
+        phone: this.data.phone
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "post",
+      success: function (res) {
+        var verificationCode = res.data.data
+        that.setData({
+          verificationCode: verificationCode
+        })
+      },
+      fail: function (err) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+      }
+    })
+  },
+  login: function () {
+    // wx.request({
+    //   url: 'http://192.168.0.143:8000/v1/member/checkUserByPhone',
+    //   data: {
+    //     phone: '15016500123',
+    //     verificationCode:'456321'
+    //   },
+    //   header: {
+    //     "Content-Type": "application/x-www-form-urlencoded"
+    //   },
+    //   method: "get",
+    //   success: function (res) {
+    //     wx.redirectTo({
+    //       url: '../index/index',
+    //     })
+    //   },
+    //   fail: function (err) { },
+    //   complete: function () { }
+    // })
+    wx.redirectTo({
+      url: '../index/index',
+    })
+  },
+  // 生命周期函数--监听页面加载
   onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    //
   }
 })
